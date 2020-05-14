4f72b2e43e23d21b244568274e921fd59b36f140
gkholman <gkenholman+github@gmail.com>
2020-05-13 21:24:22 -0400

    Comment out failing statement

--- publishUBL23Docs.sh
+++ publishUBL23Docs.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 if [ "$4" = "" ]
 then 
 echo Missing target dir, stage, dateZ, date, user, password arguments
@@ -17,7 +15,7 @@ fi
 
 echo Building package...
 java -Dant.home=utilities/ant -Xss4m -Xms400m -Xmx2000m -classpath db/spec-0.8/validate/xjparse.jar:utilities/ant/lib/ant-launcher.jar:db/spec-0.8/validate/saxon9he.jar:. org.apache.tools.ant.launch.Launcher -buildfile publishUBL23Docs.xml -Ddir=$1 -Dstage=$2 -Dversion=$3 -Ddatetimelocal=$4 -Dsetareuser=$5 -Dsetarepass=$6 | tee hub.console.$3.txt
-serverReturn=${PIPESTATUS[0]}
+# serverReturn=${PIPESTATUS[0]}
 
 if [ ! -d $1 ] then mkdir $1
 if [ ! -d $1/hub-UBL-2.3-$2-$3 ] then mkdir $1/hub-UBL-2.3-$2-$3
