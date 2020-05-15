1db854e1e668de5323cffce44adf3b60dc5845b3
gkholman <gkenholman+github@gmail.com>
2020-05-15 13:27:36 -0400

    Run bash not shell

--- publishUBL23Docs.sh
+++ publishUBL23Docs.sh
@@ -13,7 +13,7 @@ fi
 
 echo Building package...
 java -Dant.home=utilities/ant -Xss4m -Xms400m -Xmx2000m -classpath db/spec-0.8/validate/xjparse.jar:utilities/ant/lib/ant-launcher.jar:db/spec-0.8/validate/saxon9he.jar:. org.apache.tools.ant.launch.Launcher -buildfile publishUBL23Docs.xml -Ddir=$1 -Dstage=$2 -Dversion=$3 -Ddatetimelocal=$4 -Dsetareuser=$5 -Dsetarepass=$6 | tee hub.console.$3.txt
-# serverReturn=${PIPESTATUS[0]}
+serverReturn=${PIPESTATUS[0]}
 
 if [ ! -d $1 ]; then mkdir $1 ; fi
 if [ ! -d $1/hub-UBL-2.3-$2-$3 ]; then mkdir $1/hub-UBL-2.3-$2-$3 ; fi
