6264dd59c0c1fb8843803b287d6dc00082c4dfa4
gkholman <gkenholman+github@gmail.com>
2020-05-29 16:14:22 -0400

    Manually restore old develop before having corrupted it

--- publishUBL23Docs.sh
+++ publishUBL23Docs.sh
@@ -5,7 +5,7 @@ if [ -f hub.console.$3.txt ]; then rm hub.console.$3.txt ; fi
 if [ "$4" = "" ]; then echo Missing target dir, stage, dateZ, date, user, password arguments ; exit 1 ; fi
 
 echo Building package...
-java -Dant.home=utilities/ant -classpath db/spec-0.8/validate/xjparse.jar:utilities/ant/lib/ant-launcher.jar:db/spec-0.8/validate/saxon9he.jar:. org.apache.tools.ant.launch.Launcher -v -buildfile publishUBL23Docs.xml -Ddir=$1 -Dstage=$2 -Dversion=$3 -Ddatetimelocal=$4 -Dsetareuser=$5 -Dsetarepass=$6 | tee hub.console.$3.txt
+java -Dant.home=utilities/ant -classpath db/spec-0.8/validate/xjparse.jar:utilities/ant/lib/ant-launcher.jar:db/spec-0.8/validate/saxon9he.jar:. org.apache.tools.ant.launch.Launcher -buildfile publishUBL23Docs.xml -Ddir=$1 -Dstage=$2 -Dversion=$3 -Ddatetimelocal=$4 -Dsetareuser=$5 -Dsetarepass=$6 | tee hub.console.$3.txt
 serverReturn=${PIPESTATUS[0]}
 
 if [ ! -d $1 ]; then mkdir $1 ; fi
