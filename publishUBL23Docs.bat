e972219536a2275ad297ea972528276e2a5a5aa3
G. Ken Holman <gkenholman+github@gmail.com>
2020-05-29 21:28:20 -0400

    Bring master up-to-date with develop (#9)
    * Merge Peter's work into committee repository (#6)
    
    * Initial submission from TSC
    
    * UBL TSC updates to documentation
    
    * TSC first change
    
    * TSC try 2
    
    * TCS try 4
    
    * TCS try 5
    
    * Updated process descriptions in UBL.xml
    
    Co-authored-by: Peter Borresen <plb@clearviewtrade.com>
    
    * Reprocess TSC submission
    
    * Return generated entity files in result set in order to update local entities
    
    * Rerun to retrieve entities
    
    * Trigger build
    
    * Stop abort on bad validate
    
    * Debug run don't delete results
    
    * Retury to preserve outputs
    
    * Turn on verbosity for debug
    
    * Remove all abends to promote full execution
    
    * Diagnostic run
    
    * Yet more changes to handle bad inputs
    
    * Attempt to return faulty interim results
    
    * Trigger server
    
    * Try again
    
    * Manually restore old develop before having corrupted it
    
    * Minimize package size for preview
    
    * Repair echo reports
    
    * Bring DOS invocation up to date
    
    * Separate new entity files to help author regarding replacements
    
    Co-authored-by: Peter Borresen <plb@clearviewtrade.com>

--- publishUBL23Docs.bat
+++ publishUBL23Docs.bat
@@ -11,6 +11,19 @@ echo (perhaps the argument is ending in "/" when it is not supposed to)
 exit /B 1
 :resultokay
 
-echo Building package...
-java "-Dant.home=utilities/ant" -classpath "db/spec-0.8/validate/xjparse.jar;utilities/ant/lib/ant-launcher.jar;db/spec-0.8/validate/saxon9he.jar;." org.apache.tools.ant.launch.Launcher -buildfile publishUBL23Docs.xml "-Dext=bat" "-Ddir=%~1" "-Dstage=%~2" "-Dversion=%~3" "-Ddatetimelocal=%~4" "-Dsetareuser=%~5" "-Dsetarepass=%~6"
+if exist "hub.console.%~3.txt" del "hub.console.%~3.txt"
 
+echo Building package...
+java "-Dant.home=utilities/ant" -classpath "db/spec-0.8/validate/xjparse.jar;utilities/ant/lib/ant-launcher.jar;db/spec-0.8/validate/saxon9he.jar;." org.apache.tools.ant.launch.Launcher -buildfile publishUBL23Docs.xml "-Ddir=%~1" "-Dstage=%~2" "-Dversion=%~3" "-Ddatetimelocal=%~4" "-Dsetareuser=%~5" "-Dsetarepass=%~6" >"hub.console.%~3.txt"
+set serverReturn=%errorlevel%
+
+if not exist "%~1\" mkdir "%~1" 2>&1
+if not exist "%~1\hub-UBL-2.3-%~2-%~3\" mkdir "%~1\hub-UBL-2.3-%~2-%~3" 2>&1
+if not exist "%~1\hub-UBL-2.3-%~2-%~3\archive-only-not-in-final-distribution\" mkdir "%~1\hub-UBL-2.3-%~2-%~3\archive-only-not-in-final-distribution" 2>&1
+
+move "hub.console.%~3.txt" "%~1\hub-UBL-2.3-%~2-%~3\archive-only-not-in-final-distribution"
+echo %serverReturn% >"%~1\hub-UBL-2.3-%~2-%~3\archive-only-not-in-final-distribution\hub.exit.%~3.txt"
+
+pushd "%~1"
+7z a "hub-UBL-2.3-%~2-%~3.zip" "hub-UBL-2.3-%~2-%~3"
+popd
