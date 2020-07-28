ab5bdc10a88c436657ff6f3a73517e69093e6bb8
gkholman <gkenholman+github@gmail.com>
2020-07-28 13:21:45 -0400

    Rename to CSD03 for voting

--- publishUBL23Docs.sh
+++ publishUBL23Docs.sh
@@ -2,21 +2,25 @@
 
 if [ -f hub.console.$3.txt ]; then rm hub.console.$3.txt ; fi
 
-if [ "$4" = "" ]; then echo Missing target dir, stage, dateZ, date, user, password arguments ; exit 1 ; fi
+if [ "$4" = "" ]; then echo Missing target dir, platform, dateZ, date, user, password arguments ; exit 1 ; fi
+
+package=UBL-2.3
+UBLstage=csd03
+version=$3
 
 echo Building package...
-java -Dant.home=utilities/ant -classpath db/spec-0.8/validate/xjparse.jar:utilities/ant/lib/ant-launcher.jar:db/spec-0.8/validate/saxon9he.jar:. org.apache.tools.ant.launch.Launcher -buildfile publishUBL23Docs.xml -Ddir=$1 -Dstage=$2 -Dversion=$3 -Ddatetimelocal=$4 -Dsetareuser=$5 -Dsetarepass=$6 | tee hub.console.$3.txt
+java -Dant.home=utilities/ant -classpath db/spec-0.8/validate/xjparse.jar:utilities/ant/lib/ant-launcher.jar:db/spec-0.8/validate/saxon9he.jar:. org.apache.tools.ant.launch.Launcher -buildfile publishUBL23Docs.xml -Ddir=$1 -Dstage=$UBLstage -Dversion=$version -Ddatetimelocal=$4 -Dsetareuser=$5 -Dsetarepass=$6 | tee hub.console.$3.txt
 serverReturn=${PIPESTATUS[0]}
 
 if [ ! -d $1 ]; then mkdir $1 ; fi
-if [ ! -d $1/hub-UBL-2.3-$2-$3 ]; then mkdir $1/hub-UBL-2.3-$2-$3 ; fi
-if [ ! -d $1/hub-UBL-2.3-$2-$3/archive-only-not-in-final-distribution/ ]; then mkdir $1/hub-UBL-2.3-$2-$3/archive-only-not-in-final-distribution/ ; fi
-mv hub.console.$3.txt $1/hub-UBL-2.3-$2-$3/archive-only-not-in-final-distribution/
-echo $serverReturn >$1/hub-UBL-2.3-$2-$3/archive-only-not-in-final-distribution/hub.exitcode.$3.txt
+if [ ! -d $1/hub-$package-$UBLstage-$version ]; then mkdir $1/hub-$package-$UBLstage-$version ; fi
+if [ ! -d $1/hub-$package-$UBLstage-$version/archive-only-not-in-final-distribution/ ]; then mkdir $1/hub-$package-$UBLstage-$version/archive-only-not-in-final-distribution/ ; fi
+mv hub.console.$3.txt $1/hub-$package-$UBLstage-$version/archive-only-not-in-final-distribution/
+echo $serverReturn >$1/hub-$package-$UBLstage-$version/archive-only-not-in-final-distribution/hub.exitcode.$3.txt
 
 # reduce GitHub storage costs by zipping results and deleting intermediate files
 pushd $1
-7z a hub-UBL-2.3-$2-$3.zip hub-UBL-2.3-$2-$3
+7z a hub-$package-$UBLstage-$version.zip hub-$package-$UBLstage-$version
 popd
 
 if [ "$1" = "target" ]
@@ -28,7 +32,7 @@ then
 # further reduce GitHub storage costs by deleting repository files
 
 find . -not -name target -not -name .github -maxdepth 1 -exec rm -r -f {} \;
-mv $1/hub-UBL-2.3-$2-$3.zip .
+mv $1/hub-$package-$UBLstage-$version.zip .
 rm -r -f $1
 
 fi
